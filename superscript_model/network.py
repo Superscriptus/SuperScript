be92f24a4019cddf9a841e6fba579ad4e0b85cda
Chris McWilliams <chris.mcwilliams@bristol.ac.uk>
2021-03-21 07:59:52 +0000

    documented network.py

--- superscript_model/network.py
+++ superscript_model/network.py
@@ -1,4 +1,12 @@
-import pickle
+"""
+SuperScript network module
+===========
+
+Classes:
+    SocialNetwork
+        Class for tracking social network of collaborations
+"""
+
 import networkx as nx
 from itertools import combinations
 from mesa.space import NetworkGrid
@@ -7,6 +15,24 @@ from .config import HISTORICAL_SUCCESS_RATIO_THRESHOLD
 
 
 class SocialNetwork(NetworkGrid):
+    """Social network class.
+
+    Is a subclass of mesa.space.NetworkGrid
+
+    Edges in this network count the number of successful collaborations
+    between each pair of workers.
+    ...
+
+    Attributes:
+        model: model.SuperScriptModel
+            Reference to main model.
+        G: nx.Graph
+            Networkx x graph object, created in main model.
+        success_threshold: float
+            Fraction of team members who need to have worked before on
+            successful project for get_team_historical_success_flag to
+            return True.
+    """
 
     def __init__(self, model, G,
                  success_threshold=HISTORICAL_SUCCESS_RATIO_THRESHOLD):
@@ -16,16 +42,33 @@ class SocialNetwork(NetworkGrid):
         self.success_threshold = success_threshold
 
     def initialise(self):
+        """Create the network.
+
+        Adds all workers as nodes in the network.
+        Calls base class constructor and calls base class method
+        to place each worker on their node.
+        """
 
         for worker in self.model.schedule.agents:
-            self.G.add_node(worker.worker_id,
-                            department=worker.department.dept_id)
+            self.G.add_node(
+                worker.worker_id,
+                department=worker.department.dept_id
+            )
         super().__init__(self.G)
 
         for worker, node in zip(self.model.schedule.agents, self.G.nodes()):
             self.place_agent(worker, node)
 
     def replace_worker(self, old_worker, new_worker):
+        """Replaces worker in the network (called when a worker is
+        replaced due to inactivity.
+
+        Args:
+            old_worker: worker.Worker
+                Worker being replace.
+            new_worker: worker.Worker
+                Worker replacing old worker.
+        """
 
         self._remove_agent(old_worker, old_worker.worker_id)
         self.G = nx.relabel_nodes(
@@ -35,16 +78,17 @@ class SocialNetwork(NetworkGrid):
         )
         self.place_agent(new_worker, new_worker.worker_id)
 
-    # def remove_from_graph(self, worker):
-    #     self._remove_agent(worker, worker.worker_id)
-    #     self.G.remove_node(worker.worker_id)
-    #
-    # def add_to_graph(self, worker):
-    #     self.G.add_node(worker.worker_id,
-    #                     department=worker.department.dept_id)
-    #     self.place_agent(worker, self.G.nodes(worker.worker_id))
-
     def add_team_edges(self, team):
+        """Adds all edges between pairs of workers in this team.
+
+        If an edge already exists, its weight is incremented by 1.
+
+        Args:
+            team: organisation.Team
+                Provides list of members that have successfully
+                collaborated on a project and are to be added
+                to the network.
+        """
 
         pairs = list(combinations(team.members.keys(), 2))
         for pair in pairs:
@@ -55,6 +99,16 @@ class SocialNetwork(NetworkGrid):
                 self.G[pair[0]][pair[1]]['weight'] += 1
 
     def get_team_historical_success_flag(self, team):
+        """Determine is team meets required threshold for previous
+        successful collaboration.
+
+        Args:
+            team: organisation.Team
+
+        Returns:
+            bool: True if more than self.success_threshold of possible
+                  edges are realised in this network.
+        """
 
         pairs = list(combinations(team.members.keys(), 2))
         success_ratio = 0
@@ -71,6 +125,7 @@ class SocialNetwork(NetworkGrid):
             return False
 
     def save(self):
+        """Save network for later analysis."""
         nx.write_gpickle(
             self.G,
             self.model.io_dir
