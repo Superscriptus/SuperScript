6f1f8744c3f9f78d67b1a31f729d10f4ae235a28
rustyBilges <tophermcw@gmail.com>
2021-09-05 10:16:25 +0100

    finished and tested ROI script (for cumulative total roi)

--- analysis/retrospective_metrics/roi.py
+++ analysis/retrospective_metrics/roi.py
@@ -11,6 +11,7 @@ will be included as standard.
 import pandas as pd
 import pickle
 import numpy as np
+import matplotlib.pyplot as plt
 
 HARD_SKILLS = ['A', 'B', 'C', 'D', 'E']
 
@@ -28,13 +29,26 @@ def load_data(filepath):
 
 def get_projects_for_worker(worker_id, timestep, worker_data):
 
-    if worker_data.loc[timestep, worker_id].contributes is not None:
-        contributions = [worker_data.loc[timestep, worker_id].contributes[skill] for skill in HARD_SKILLS]
+    contributions = worker_data.loc[timestep, worker_id].contributes
+    if contributions is not None:
+        contributions = [contributions[skill] for skill in HARD_SKILLS]
         worker_projects = list(set([item for sublist in contributions for item in sublist]))
         return worker_projects
 
     else:
         return []
+
+
+def get_workers_for_project(pid, timestep, worker_data):
+    workers_present = worker_data.loc[timestep, :].index
+
+    workers = []
+    for worker_id in workers_present:
+        worker_projects = get_projects_for_worker(worker_id, timestep, worker_data)
+        if pid in worker_projects:
+            workers.append(worker_id)
+
+    return workers
 
 
 def get_running_projects(timestep, worker_data):
@@ -55,9 +69,71 @@ def completed_projects(timestep, worker_data):
         return None
 
 
+def get_project_status(pid, project_data):
+    return project_data.loc[pid].success
+
+
+def get_return(outcome, return_dict={True: 25, False: 5}):
+    if outcome is None:
+        return None
+    else:
+        return return_dict[outcome]
+
+
+def calculate_total_cummulative_roi(worker_data, project_data):
+
+    roi_worker_dict = dict()
+    roi_total_dict = dict()
+
+    for t in range(1, 101):
+        print(t)
+
+        roi_worker_dict[t] = {}
+        roi_total_dict[t] = 0
+        if t > 1:
+            roi_total_dict[t] += roi_total_dict[t - 1]
+
+        completed = completed_projects(t, worker_data)
+        if completed is not None:
+            status = [get_return(get_project_status(p, project_data)) for p in completed_projects(t, worker_data)]
+        else:
+            status = None
+        # print(completed_projects(t, agents), status)
+
+        if completed is not None and len(completed) > 0:
+
+            for p in completed:
+                status = get_return(get_project_status(p, project_data))
+                p_worker = get_workers_for_project(p, t - 1, worker_data)
+
+                for w in p_worker:
+                    roi_total_dict[t] += status
+                # print(get_workers_for_project(p, t-1, agents))
+
+    return roi_total_dict
+
+
 if __name__ == "__main__":
 
     replicate = 0
+    agents_f = '../../simulation_io/project_per_step_2_230521_v1.0/Random/agents_vars_rep_%d.pickle' % replicate
+    projects_f = '../../simulation_io/project_per_step_2_230521_v1.0/Random/projects_table_rep_%d.pickle' % replicate
+
+    agents = load_data(agents_f)
+    projects = load_data(projects_f)
+
+    #print(agents.head())
+    # print(projects.head())
+    #print(projects.columns)
+    # print(len(projects))
+    # print(agents.columns)
+    #
+    # print(get_projects_for_worker(4, 1, agents))
+    # print(get_projects_for_worker(4, 4, agents))
+    # print(get_projects_for_worker(4, 5, agents))
+
+    roi_tot_r = calculate_total_cummulative_roi(agents, projects)
+
     agents_f = '../../simulation_io/project_per_step_2_230521_v1.0/Basin_w_flex/agents_vars_rep_%d.pickle' % replicate
     projects_f = '../../simulation_io/project_per_step_2_230521_v1.0/Basin_w_flex/projects_table_rep_%d.pickle' % replicate
 
@@ -74,9 +150,16 @@ if __name__ == "__main__":
     # print(get_projects_for_worker(4, 4, agents))
     # print(get_projects_for_worker(4, 5, agents))
 
-    for t in range(1, 101):
-        #print(get_running_projects(t, agents))
-        print(completed_projects(t, agents))
+    roi_tot_bwf = calculate_total_cummulative_roi(agents, projects)
+
+    plt.plot(list(roi_tot_bwf.values()), label='Basin_w_flex')
+    plt.plot(list(roi_tot_r.values()), label='Random')
+    plt.legend()
+    plt.title('Cumulative Total Return on Investment')
+    plt.xlabel('time')
+    plt.ylabel('cumulative ROI')
+    plt.show()
+            # NEXT: save worker ROI in dict, and average (mean or median) ROI for present workers.
         #break
 
 
