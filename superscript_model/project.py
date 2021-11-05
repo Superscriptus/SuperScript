ad4b2486e963b8973b6c53eea281c1e24e8fef6c
Chris McWilliams <chris.mcwilliams@bristol.ac.uk>
2021-11-05 18:20:38 +0000

    added temporary baseline adjustment to probability calculator

--- superscript_model/project.py
+++ superscript_model/project.py
@@ -948,7 +948,10 @@ class SuccessCalculator:
                 + self.probability_risk.get_values(self.risk)
                 + self.probability_chemistry.get_values(self.chemistry)
             ) / 100
-            project.success_probability = max(0, probability)
+            #project.success_probability = max(0, probability)
+            # Temporary adjustment: add baseline....
+            probability += 0.2
+            project.success_probability = max(0, min(1.0, probability))
 
     def determine_success(self, project):
         """Determine if the project was successful.
