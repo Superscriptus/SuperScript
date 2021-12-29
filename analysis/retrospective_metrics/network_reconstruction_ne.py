8adcbf22112d96fb46be5f84ee8778f4a8d7ce46
Chris McWilliams <chris.mcwilliams@bristol.ac.uk>
2021-12-29 11:19:31 +0000

    updated network script

--- analysis/retrospective_metrics/network_reconstruction_ne.py
+++ analysis/retrospective_metrics/network_reconstruction_ne.py
@@ -276,8 +276,8 @@ def run_network_reconstruction_for_preset_e(sim_path='../../simulation_io/stream
 
 if __name__ == "__main__":
 
-    run_network_reconstruction_for_all_simulations()
-    # run_network_reconstruction_for_preset_e()
+    # run_network_reconstruction_for_all_simulations()
+    run_network_reconstruction_for_preset_e()
 
     parameter_combinations = [
         [10, 0.95, 0.3, 0.3, 1],
